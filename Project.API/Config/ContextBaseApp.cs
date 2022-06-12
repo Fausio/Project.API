eaa4029809c59f13951468c4ec9db36b1f2df7d8
fausio luis <fausioluis@live.com>
2022-06-12 11:16:52 +0200

    ContextBaseApp

---
+++ Project.API/Config/ContextBaseApp.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Project.API.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.API.Config
+{
+    public class ContextBaseApp : IdentityDbContext<ApplicationUser>
+    {
+
+        public ContextBaseApp(DbContextOptions<ContextBaseApp> options) : base(options)
+        {
+
+        }
+    }
+}
