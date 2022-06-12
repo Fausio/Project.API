4079c5ad45d7fc70807b765d6c0e73cdf2eca314
fausio luis <fausioluis@live.com>
2022-06-12 12:37:06 +0200

    add RepositoryProduct

---
+++ Project.API/Repository/RepositoryProduct.cs
@@ -0,0 +1,15 @@
+﻿using Project.API.Repository;
+using Project.Data.Entity;
+using Project.Data.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Data.Repository
+{
+    public class RepositoryProduct: RepositoryGeneric<Product> , IProduct
+    {
+    }
+}
