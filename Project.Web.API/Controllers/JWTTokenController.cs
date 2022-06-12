13ad5fe683994440bfd1511c9ad0cb9ab777a783
fausio luis <fausioluis@live.com>
2022-06-12 13:10:19 +0200

    CreateToken in contrler

---
+++ Project.Web.API/Controllers/JWTTokenController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Project.Data.Entity;
+using Project.Data.Entity.Model;
+using Project.Web.API.Token;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Web.API.Controllers
+{
+    public class JWTTokenController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public JWTTokenController(SignInManager<ApplicationUser> signInManager, 
+                                  UserManager<ApplicationUser>  userManager
+                                )
+        {
+            this._signInManager = signInManager;
+            this._userManager = userManager;
+        }
+       
+        [AllowAnonymous]
+        [Produces("application/json")]
+        [HttpPost("/api/CreateToken")]
+
+        public async Task<IActionResult> CreateToken([FromBody] ImputModel input)
+        {
+            if (string.IsNullOrWhiteSpace(input.Email) || string.IsNullOrWhiteSpace(input.Password))
+            {
+                return Unauthorized();
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(input.Email, input.Password, false, false);
+            if (result.Succeeded)
+            {
+                var token = new TokenJWTBuilder().AddSecurityKey(JWTSecurityKey.Create("Secret_key-12345678"))
+                                                 .AddSubject("Genesys Code")
+                                                 .AddIssuer("Test.Security.Bearer")
+                                                 .AddAudience("test.Security.Bearer")
+                                                 .AddClaim("UserAPINumber", "1")
+                                                 .AddExpiry(5)
+                                                 .Builder();
+                return Ok(token.value);
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
