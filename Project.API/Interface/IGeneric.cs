e3e99369a2eee80e3aa296bc54d331955e5ad52b
fausio luis <fausioluis@live.com>
2022-06-12 11:40:04 +0200

    interface IGeneric

---
+++ Project.API/Interface/IGeneric.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.API.Interface
+{
+    public interface IGeneric<T> where T : class
+    {
+        Task Create(T obj);
+        Task Update(T obj);
+        Task Delete(T obj);
+        Task Get(T obj);
+        Task<List<T>> Get();
+    }
+}
