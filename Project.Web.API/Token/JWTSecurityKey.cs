e269479d262960574318e046194850870ef211fd
fausio luis <fausioluis@live.com>
2022-06-12 12:42:31 +0200

    JWTSecurityKey.SymmetricSecurityKey

---
+++ Project.Web.API/Token/JWTSecurityKey.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Project.Web.API.Token
+{
+    public class JWTSecurityKey
+    {
+        public static SymmetricSecurityKey Create(string secrest)
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secrest));
+        }
+    }
+}
